384227a7edf265c218d697f253c091302fadccfe
praveenallamz <38095063+praveenallamz@users.noreply.github.com>
2019-08-06 15:49:47 +0530

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -8,7 +8,8 @@ public class Test {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		System.setProperty("webdriver.chrome.driver","chromedriver");
+		//System.setProperty("webdriver.chrome.driver","chromedriver");
+		WebDriver driver = new RemoteWebDriver("http://localhost:9515", DesiredCapabilities.chrome());
 		  driver = new ChromeDriver();
 		
 		  driver.manage().window().maximize();
